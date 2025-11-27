66e93c4fa703baed738578d52f882750778e1096
rootmium <rootmium@gmail.com>
2025-11-27 16:11:09 +0600

    feat(emacs): basic settings and modes

--- .config/emacs/root/root-base-config.el
+++ .config/emacs/root/root-base-config.el
@@ -1,3 +1,61 @@
+;; Load a dark theme
 (load-theme 'modus-vivendi)
+
+;; Only ask y or n
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Core settings
+(setq inhibit-startup-screen t
+      inhibit-startup-message t
+      initial-scratch-message ";; Emacs here!"
+      auto-save-default nil
+      make-backup-files nil
+      large-file-warning-threshold nil
+      vc-follow-symlinks t
+      global-auto-revert-non-file-buffers t
+      native-comp-async-report-warnings-errors nil
+      help-window-select t
+      recentf-max-saved-items 2000
+      undo-limit 100000000
+      scroll-conservatively 101
+      scroll-step 1
+      scroll-margin 5
+      )
+
+;; Modes
+(savehist-mode 1)
+(column-number-mode 1)
+(global-auto-revert-mode 1)
+(recentf-mode 1)
+(save-place-mode 1)
+
+;; Special modes for programming and writing
+(dolist (mode '(text-mode-hook
+                prog-mode-hook
+                org-mode-hook
+                conf-mode-hook))
+  (add-hook mode (lambda ()
+		   (display-line-numbers-mode 1)
+                   (hl-line-mode 1)
+                   (display-fill-column-indicator-mode 1)
+                   (electric-pair-mode 1)
+                   (show-paren-mode)
+                   )))
+
+;; Show vertical column
+(setq-default display-fill-column-indicator-column 79)
+
+;; Tabs to spaces
+(setq-default indent-tabs-mode nil
+	            tab-width 2)
+
+;; Periodicly save recent files
+(run-with-idle-timer 600 t (lambda ()
+                             (let ((save-silently t))
+                               (recentf-save-list))))
+
+;; Save custom variables somewhere else
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file 'noerror 'nomessage)
 
 (provide 'root-base-config)
