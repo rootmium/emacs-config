4185d5e0bca4ab5f542bf1cfc4a32ecbf701bc55
rootmium <rootmium@gmail.com>
2025-11-28 12:41:15 +0600

    feat(emacs): keybind for terminal

--- .config/emacs/root/root-keybinds.el
+++ .config/emacs/root/root-keybinds.el
@@ -1,5 +1,6 @@
 (root/global-keymap "C-S-c" 'kill-ring-save
 		                 "C-S-v" 'yank
-                     "M-;" 'comment-line)
+                     "M-;" 'comment-line
+                     "C-c t" #'(lambda () (interactive) (term "/bin/zsh")))
 
 (provide 'root-keybinds)
