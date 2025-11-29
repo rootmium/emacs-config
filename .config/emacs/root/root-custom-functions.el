de6a008fc1fc23e2bfd1314ff4d71246feeef189
rootmium <rootmium@gmail.com>
2025-11-29 15:52:00 +0600

    feat(emacs): function to paste(yank) in term-char-mode

--- .config/emacs/root/root-custom-functions.el
+++ .config/emacs/root/root-custom-functions.el
@@ -5,4 +5,14 @@
       (keymap-global-set (car k+c) (cadr k+c))
       (setq k+c  (cddr k+c)))))
 
+;; Custom function to paste in the terminal buffer
+(defun root/term-yank ()
+  "Switch to `term-line-mode' paste the text
+then switch back to `term-char-mode'"
+  (interactive)
+  (term-line-mode)
+  (yank)
+  (term-char-mode)
+  )
+
 (provide 'root-custom-functions)
