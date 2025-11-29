54894ee758c315deff77258c263384483edf40f6
rootmium <rootmium@gmail.com>
2025-11-29 16:29:42 +0600

    feat(emacs): vi like keybinds to move between windows

--- .config/emacs/root/root-keybinds.el
+++ .config/emacs/root/root-keybinds.el
@@ -2,6 +2,12 @@
 		                 "C-S-v" 'yank
                      "M-;" 'comment-line
                      "C-x C-b" 'nil
+
+                     ;; Switch windows with Meta(Alt) + hjkl
+                     "M-h" 'windmove-left
+                     "M-l" 'windmove-right
+                     "M-k" 'windmove-up
+                     "M-j" 'windmove-down
                      )
 
 (provide 'root-keybinds)
