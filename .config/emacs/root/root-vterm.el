1a35b0c25d6a161bbcec0536400a5749c9bc929b
rootmium <rootmium@gmail.com>
2025-11-29 16:30:40 +0600

    fix(emacs): similar window move keybinds in vterm insert mode

--- .config/emacs/root/root-vterm.el
+++ .config/emacs/root/root-vterm.el
@@ -1,4 +1,12 @@
-(use-package vterm)
+(use-package vterm
+  :config
+  ;; Switch windows in insert mode
+  (evil-define-key 'insert vterm-mode-map
+    (kbd "M-h") 'windmove-left
+    (kbd "M-l") 'windmove-right
+    (kbd "M-k") 'windmove-up
+    (kbd "M-j") 'windmove-down)
+  )
 
 (root/global-keymap "C-c t" 'vterm)
 
