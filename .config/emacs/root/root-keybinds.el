d51c7fb99f415d3b9daef99b0b48042de59235e5
rootmium <rootmium@gmail.com>
2025-11-29 15:51:06 +0600

    fix(emacs): disable annoying C-x C-b keybind

--- .config/emacs/root/root-keybinds.el
+++ .config/emacs/root/root-keybinds.el
@@ -1,6 +1,7 @@
 (root/global-keymap "C-S-c" 'kill-ring-save
 		                 "C-S-v" 'yank
                      "M-;" 'comment-line
+                     "C-x C-b" 'nil
                      )
 
 (provide 'root-keybinds)
