d65dad633ae35e08d7212123c501e635987df349
rootmium <rootmium@gmail.com>
2025-11-29 15:52:38 +0600

    feat(emacs): keybind to paste in term-mode

--- .config/emacs/root/root-multi-term.el
+++ .config/emacs/root/root-multi-term.el
@@ -4,4 +4,7 @@
 ;; Global keybind to start terminals
 (root/global-keymap "C-c t" 'multi-term)
 
+;; Paste in the terminal buffer
+(add-to-list 'term-bind-key-alist '("C-S-v" . root/term-yank))
+
 (provide 'root-multi-term)
