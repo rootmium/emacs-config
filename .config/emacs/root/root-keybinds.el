30dbdd98e0e2328df2bebd694586593be5d23502
rootmium <rootmium@gmail.com>
2025-11-29 15:50:45 +0600

    fix(emacs): remove conflicting keybind with multi-term

--- .config/emacs/root/root-keybinds.el
+++ .config/emacs/root/root-keybinds.el
@@ -1,6 +1,6 @@
 (root/global-keymap "C-S-c" 'kill-ring-save
 		                 "C-S-v" 'yank
                      "M-;" 'comment-line
-                     "C-c t" #'(lambda () (interactive) (term "/bin/zsh")))
+                     )
 
 (provide 'root-keybinds)
