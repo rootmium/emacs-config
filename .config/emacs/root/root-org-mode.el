5a9650a9854ded4c99c5af37be2bde32f9f13f0c
rootmium <rootmium@gmail.com>
2025-11-30 19:58:12 +0600

    feat(emacs): window movement keybinds for org-mode

--- .config/emacs/root/root-org-mode.el
+++ .config/emacs/root/root-org-mode.el
@@ -7,6 +7,10 @@
    'org-babel-load-languages
    '((emacs-lisp . t)
      (http . t)))
+  (define-key org-mode-map (kbd "M-h") 'windmove-left)
+  (define-key org-mode-map (kbd "M-l") 'windmove-right)
+  (define-key org-mode-map (kbd "M-k") 'windmove-up)
+  (define-key org-mode-map (kbd "M-j") 'windmove-down)
   )
 
 (provide 'root-org-mode)
