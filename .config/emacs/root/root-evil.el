976221162de103b3788b130e43a95c5135e4962e
rootmium <rootmium@gmail.com>
2025-11-30 20:01:07 +0600

    feat(emacs): keybinds to move between windows

--- .config/emacs/root/root-evil.el
+++ .config/emacs/root/root-evil.el
@@ -17,6 +17,15 @@
   (define-key evil-motion-state-map (kbd "0") 'evil-first-non-blank)
   (define-key evil-visual-state-map (kbd "0") 'evil-first-non-blank)
 
+  (evil-define-key 'normal org-mode-map (kbd "M-h") 'windmove-left)
+  (evil-define-key 'normal org-mode-map (kbd "M-j") 'windmove-down)
+  (evil-define-key 'normal org-mode-map (kbd "M-k") 'windmove-up)
+  (evil-define-key 'normal org-mode-map (kbd "M-l") 'windmove-right)
+
+  (evil-define-key 'insert org-mode-map (kbd "M-h") 'windmove-left)
+  (evil-define-key 'insert org-mode-map (kbd "M-j") 'windmove-down)
+  (evil-define-key 'insert org-mode-map (kbd "M-k") 'windmove-up)
+  (evil-define-key 'insert org-mode-map (kbd "M-l") 'windmove-right)
   (evil-define-key 'normal dired-mode-map
     "h" 'dired-up-directory
     "l" 'dired-find-file)
