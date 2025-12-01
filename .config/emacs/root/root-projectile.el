25c2a3259592fc177e919f02ddf9dfe357e236a4
rootmium <rootmium@gmail.com>
2025-12-01 18:05:57 +0600

    fix(emacs): index other dev project

--- .config/emacs/root/root-projectile.el
+++ .config/emacs/root/root-projectile.el
@@ -1,7 +1,7 @@
 (use-package projectile
   :ensure t
   :init
-  (setq projectile-project-search-path '("~/Projects/git-repos/github/"))
+  (setq projectile-project-search-path '("~/Projects/git-repos/github/" "~/Projects/git-repos/github/backend-dev-log"))
   :config
   ;; On Linux, however, I usually go with another one
   (define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
