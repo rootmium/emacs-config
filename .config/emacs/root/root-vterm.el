788548442ecb89a84347acf5650e481e7e48f488
rootmium <rootmium@gmail.com>
2025-12-01 18:00:03 +0600

    fix(emacs): send some keys directly to vterm

--- .config/emacs/root/root-vterm.el
+++ .config/emacs/root/root-vterm.el
@@ -1,11 +1,22 @@
 (use-package vterm
   :config
-  ;; Switch windows in insert mode
   (evil-define-key 'insert vterm-mode-map
+    ;; Switch windows in insert mode
     (kbd "M-h") 'windmove-left
     (kbd "M-l") 'windmove-right
     (kbd "M-k") 'windmove-up
-    (kbd "M-j") 'windmove-down)
+    (kbd "M-j") 'windmove-down
+
+    ;; Send these keys directly to vterm
+    (kbd "C-e") #'vterm--self-insert
+    (kbd "C-f") #'vterm--self-insert
+    (kbd "C-a") #'vterm--self-insert
+    (kbd "C-v") #'vterm--self-insert
+    (kbd "C-b") #'vterm--self-insert
+    (kbd "C-n") #'vterm--self-insert
+    (kbd "C-p") #'vterm--self-insert
+    (kbd "C-r") #'vterm--self-insert
+    (kbd "C-t") #'vterm--self-insert
   )
 
 ;; Keybind for vterm
