c7d8c934a40a16b0464c4789b7ff49e9efa0f8a9
rootmium <rootmium@gmail.com>
2025-12-02 19:22:33 +0600

    fix(emacs): removing evil

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -13,7 +13,6 @@
 (require 'root-doom-modeline)
 (require 'root-exec-path-from-shell)
 (require 'root-undo-tree)
-(require 'root-evil)
 (require 'root-diff-hl)
 (require 'root-vterm)
 (require 'root-magit)
