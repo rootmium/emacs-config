48c92f9d61f81d325309a66bf616e5494af26e2d
rootmium <rootmium@gmail.com>
2025-12-21 20:37:49 +0600

    feat(emacs): todo and agenda config for org-mode

--- .config/emacs/root/root-org-mode.el
+++ .config/emacs/root/root-org-mode.el
@@ -9,4 +9,43 @@
      (http . t)))
   )
 
+;; Directory for org files
+(setq org-directory "~/Projects/org")
+(setq org-agenda-files (list org-directory))
+
+;; Custom TODO keywords
+(setq org-todo-keywords
+      '((sequence "TODO(t)" "NEXT(n)" "STARTED(s)" "|" "DONE(d)")
+        (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)")))
+
+;; Colors for keywords
+(setq org-todo-keyword-faces
+      '(("NEXT" . "orange") ("STARTED" . "yellow")
+        ("WAITING" . "purple") ("HOLD" . "magenta")))
+
+;; Log time when a task is completed
+(setq org-log-done 'time)
+(setq org-log-into-drawer t)
+
+(setq org-default-notes-file (concat org-directory "/inbox.org"))
+
+(setq org-capture-templates
+      '(("t" "Todo" entry (file+headline "inbox.org" "Tasks")
+         "* TODO %?\n  %i\n  %a")
+        ("j" "Journal" entry (file+datetree "journal.org")
+         "* %?\nEntered on %U\n  %i\n  %a")
+        ("m" "Meeting" entry (file "meetings.org")
+         "* MEETING with %? :MEETING:\n%U")))
+
+(global-set-key (kbd "C-c i") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+;; Refile tasks easily to other files
+(setq org-refile-targets
+      '((nil :maxlevel . 3)
+        (org-agenda-files :maxlevel . 3)))
+(setq org-outline-path-complete-in-steps nil)
+(setq org-refile-use-outline-path 'file)
+
 (provide 'root-org-mode)
