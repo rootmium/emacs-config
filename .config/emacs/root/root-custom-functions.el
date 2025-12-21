fd2fd3e44125672d8659a084f7e4785f07a016b1
rootmium <rootmium@gmail.com>
2025-12-21 20:36:03 +0600

    feat(emacs): custom function to upload file on 0x0.st

--- .config/emacs/root/root-custom-functions.el
+++ .config/emacs/root/root-custom-functions.el
@@ -15,4 +15,32 @@ then switch back to `term-char-mode'"
   (term-char-mode)
   )
 
+;; Upload selected file to 0x0
+(defun root/upload-to-0x0 ()
+  "Upload file to 0x0.st
+The URL is saved to the clipboard upon completion"
+  (interactive)
+  (let* ((file (expand-file-name (read-file-name "File to upload: ")))
+         (file-name (file-name-nondirectory file))
+         (process-buffer (generate-new-buffer (format " *0x0-upload-%s*" file-name))))
+    (message "Uploading %s..." file-name)
+
+    (make-process
+     :name "0x0-upload"
+     :buffer process-buffer
+     :command (list "curl" "-sF" (format "file=@%s" file) "-Fexpires=24" "https://0x0.st")
+     :sentinel
+     (lambda (proc event)
+       (when (string-equal event "finished\n")
+         (let ((url (with-current-buffer (process-get proc :output-buffer)
+                      (string-trim (buffer-string)))))
+           (if (string-match-p "https://0x0\\.st/" url)
+               (progn
+                 (kill-new url)
+                 (message "Upload finished! (24H) URL copied: %s" url))
+             (message "Upload failed: %s" url))
+           (kill-buffer (process-get proc :output-buffer))))))
+    (process-put (get-process "0x0-upload") :output-buffer process-buffer)
+    nil))
+
 (provide 'root-custom-functions)
