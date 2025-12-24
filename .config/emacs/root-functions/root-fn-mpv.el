19a2a377cf84690987e61bf7c7a88d0c30d5d328
rootmium <rootmium@gmail.com>
2025-12-24 12:09:59 +0600

    fix(emacs): docstring and input fixes

--- .config/emacs/root-functions/root-fn-mpv.el
+++ .config/emacs/root-functions/root-fn-mpv.el
@@ -71,10 +71,15 @@ and deletes it if found."
     (message "No mpv process is running.")))
 
 (defun root/mpv-shuffle-dir (dir)
-  "Launch mpv in a comint buffer to play files in DIR in shuffle mode."
-  (interactive "DSelect directory to play: ")
+  "Launch mpv in a comint buffer to play files in DIR in shuffle mode.
+
+This function uses `root/music-directory' as base path. Any existing mpv
+process is killed with `root/stop-mpv' before starting the new one."
+  (interactive
+   (list (read-directory-name "Select directory to play: " root/music-directory)))
   (let ((dir-path (expand-file-name dir)))
-    (make-comint-in-buffer "mpv-shuffle" "*mpv-output*" "mpv" nil "--shuffle" dir-path)
+    (root/stop-mpv)
+    (make-comint-in-buffer root/mpv--process-name "*mpv-output*" "mpv" nil "--shuffle" dir-path)
     (message "Starting mpv shuffle in %s..." dir)))
 
 (defun root/get-current-media-title ()
