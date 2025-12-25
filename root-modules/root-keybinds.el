9890454f51e125a35a796a934f66964304243c01
rootmium <rootmium@gmail.com>
2025-12-25 20:43:52 +0600

    refactor: properly indent

--- root-modules/root-keybinds.el
+++ root-modules/root-keybinds.el
@@ -1,13 +1,13 @@
 (root/global-keymap "C-S-c" 'kill-ring-save
-		                 "C-S-v" 'yank
-                     "M-;" 'comment-line
-                     "C-x C-b" 'nil
-                     "C-c t" '(lambda () (interactive) (ansi-term "/bin/zsh"))
-                     "C-c m m" 'root/mpv-menu
-                     "M-o" 'pop-to-mark-command
-                     "C-c b" 'ibuffer
-                     "C-c e d" 'root/duplicate-line-or-region
-                     "C-c e f" 'root/indent-clean-buffer
-                      )
+		                "C-S-v" 'yank
+                    "M-;" 'comment-line
+                    "C-x C-b" 'nil
+                    "C-c t" '(lambda () (interactive) (ansi-term "/bin/zsh"))
+                    "C-c m m" 'root/mpv-menu
+                    "M-o" 'pop-to-mark-command
+                    "C-c b" 'ibuffer
+                    "C-c e d" 'root/duplicate-line-or-region
+                    "C-c e f" 'root/indent-clean-buffer
+                    )
 
 (provide 'root-keybinds)
